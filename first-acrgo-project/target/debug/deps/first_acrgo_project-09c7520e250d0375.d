cb1d1e05d49ef74a23e5d5313ee203c8cb4bdc9d
JeannotMN <jeannot@ssquared.dev>
2026-04-10 09:00:25 +0200

    Revert "cargo new"
    This reverts commit ccb694967b6550cfedceebdae3f01a1875d7242a.

--- first-acrgo-project/target/debug/deps/first_acrgo_project-09c7520e250d0375.d
+++
@@ -1,5 +0,0 @@
-/Users/jeannot/code/rusty/first-acrgo-project/target/debug/deps/first_acrgo_project-09c7520e250d0375.d: src/main.rs
-
-/Users/jeannot/code/rusty/first-acrgo-project/target/debug/deps/first_acrgo_project-09c7520e250d0375: src/main.rs
-
-src/main.rs:
--- first-acrgo-project/target/debug/first-acrgo-project.d
+++
@@ -1,1 +0,0 @@
-/Users/jeannot/code/rusty/first-acrgo-project/target/debug/first-acrgo-project: /Users/jeannot/code/rusty/first-acrgo-project/src/main.rs
