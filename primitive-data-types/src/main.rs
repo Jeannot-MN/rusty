89cea5413ce8a21f5eda96b0a6a90e5ae28b331e
JeannotMN <jeannot@ssquared.dev>
2026-04-10 09:29:04 +0200

    boolean bool

--- primitive-data-types/src/main.rs
+++ primitive-data-types/src/main.rs
@@ -6,10 +6,19 @@ fn main() {
     println!("Signed: {}", x);
     println!("Unsigned: {}", y);
 
+    
     // Floating-point numbers
     let pi: f32 = 3.14;
     let e: f64 = 2.71828;
 
     println!("Pi: {}", pi);
     println!("e: {}", e);
+
+
+    // Booleans
+    let is_rust_fun: bool = true;
+    let is_coding_hard: bool = false;
+
+    println!("Is Rust fun? {}", is_rust_fun);
+    println!("Is coding hard? {}", is_coding_hard);
 }
