582eaa29cc54631d8c9c0ad912b1812e4f4774f7
JeannotMN <jeannot@ssquared.dev>
2026-04-10 09:26:51 +0200

    floats f32, f64

--- primitive-data-types/src/main.rs
+++ primitive-data-types/src/main.rs
@@ -1,7 +1,15 @@
 fn main() {
+    // Integers
     let x: i32 = -5;
     let y: u32 = 10;
 
     println!("Signed: {}", x);
     println!("Unsigned: {}", y);
+
+    // Floating-point numbers
+    let pi: f32 = 3.14;
+    let e: f64 = 2.71828;
+
+    println!("Pi: {}", pi);
+    println!("e: {}", e);
 }
